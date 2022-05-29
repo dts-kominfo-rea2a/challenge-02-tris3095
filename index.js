027bce895548069d42c507b498f11389b742750a
Tris <triskominfoss@gmail.com>
2022-05-29 14:39:48 +0700

    tantangan 6

--- index.js
+++ index.js
@@ -23,20 +23,32 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
-
+  let hasilLooping = [];
+  let element = "";
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    element = dataYangAkanDilooping[index].namaDepan + " " + dataYangAkanDilooping[index].namaBelakang;
+    hasilLooping.push(element);
+  }
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
-
+  let jumlahPria = 0;
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin === "M") {
+      jumlahPria += 1;
+    }
+  }
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
-
+  let jumlahWanita = 0;
+  for (let index = 0; index < dataYangAkanDilooping.length; index++) {
+    if (dataYangAkanDilooping[index].jenisKelamin === "F") {
+      jumlahWanita += 1;
+    }
+  }
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -48,7 +60,14 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +84,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
